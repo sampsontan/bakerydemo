f40ac352aa0469ea122c696e4ab592f0e982a307
sampsontan <sampsontan@users.noreply.github.com>
2021-01-01 19:02:40 +0800

    Update base.py

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -185,3 +185,5 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
